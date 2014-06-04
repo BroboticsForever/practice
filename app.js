8ba1ed37deb07830f7d651634cdc4a3af5dc8e23
Ian Tait <ian.tait@c2fo.com>
2014-06-04 14:03:23 -0500

    Added port to config file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require("express"), //include express
-    winston = require("winston"), //include winston
     app = express(), //create our app
     page1Router = require("./routers/page1Router"), //create a router for page1
-    middleware = require("./middleware/index"); //include middleware
+    middleware = require("./middleware/index"), //include middleware
+    config = require("./config/config").config;
 
 app.use('*', middleware.authenticate); //set to authenticate on every page
 
 app.use('/', page1Router); //
 
-app.listen(3030);
+app.listen(config.ListenPort);
