3d9cc36de2658e03cc258f1702fbceaa6ada0133
Ian Tait <ian.tait@c2fo.com>
2014-06-04 11:43:21 -0500

    Added basic install and git instructions

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require("express"),
-    winston = require("winston"),
-    app = express(),
-    page1Router = require("./routers/page1Router"),
-    middleware = require("./middleware/index");
+var express = require("express"), //include express
+    winston = require("winston"), //include winston
+    app = express(), //create our app
+    page1Router = require("./routers/page1Router"), //create a router for page1
+    middleware = require("./middleware/index"); //include middleware
 
-app.use('*', middleware.authenticate);
+app.use('*', middleware.authenticate); //set to authenticate on every page
 
-app.use('/', page1Router);
+app.use('/', page1Router); //
 
 app.listen(3030);
--- config/README.md
+++ config/README.md
@@ -1,3 +1,5 @@
 Config
 ========
-These are the configurations for our project
+These are the configurations for our project.
+The config.json file is what actually contains the key value pairs of configuration information.
+The config.js file is what converts config.json into a javascript object.
