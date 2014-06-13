6d6d7a973af9c215332d50942a5768713e2468fc
Ian Tait <ian.tait@c2fo.com>
2014-06-13 09:51:12 -0500

    Fixed Handlebars functionality

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require("express"), //include express
+    expressHB = require("express3-handlebars"),
     app = express(), //create our app
     page1Router = require("./routers/page1Router"), //create a router for page1
     middleware = require("./middleware/index"), //include middleware
-    config = require("./config/config").config,
-    consolidate = require('consolidate');
+    config = require("./config/config").config;
 
-app.set("view options", { layout:false });
-app.engine('.handlebars', consolidate.handlebars);
+app.use(express.static("./public"));
+
+app.engine('handlebars', expressHB({ defaultLayout: 'main', extname: '.handlebars' }));
+app.set('view engine', 'handlebars');
 
 app.use('*', middleware.authenticate); //set to authenticate on every page
 
