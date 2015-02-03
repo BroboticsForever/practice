54cd9581c88410fd3240693c5dcb63da3310e678
Ian Tait <thetaiter@ku.edu>
2015-02-03 16:40:44 -0600

    Updated template with customizations

--- config/db.js
+++ config/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-	url : 'mongodb://<user>:<pass>@mongo.onmodulus.net:27017/uw45mypu'
+    url: 'mongodb://broDBuser:Br0b0tics@ds039441.mongolab.com:39441/brobotics-main'
 }
--- server.js
+++ server.js
@@ -1,30 +1,47 @@
-// modules =================================================
-var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
+var cluster = require('cluster');
+var env = process.env.NODE_ENV || 'development';
 
-// configuration ===========================================
-	
-// config files
-var db = require('./config/db');
+if ((cluster.isMaster) && process.argv.indexOf('--singleProcess') < 0 && env != 'development') {
+    console.log("Forking processes for each CPU...");
 
-var port = process.env.PORT || 8080; // set our port
-// mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
+    var numCPUs = require('os').cpus().length;
 
-// get all data/stuff of the body (POST) parameters
-app.use(bodyParser.json()); // parse application/json 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+    for (var i = 0; i < numCPUs; i++) {
+        console.log('Forking worker ' + i);
+        cluster.fork();
+    }
 
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+    cluster.on('exit', function(worker) {
+        console.log('Worker ' + worker.id + ' has died. Forking new worker.');
+        cluster.fork();
+    });
+} else {
+    var workerId = 0;
+    if (!cluster.isMaster) {
+        workerId = cluster.worker.id;
+    }
 
-// routes ==================================================
-require('./app/routes')(app); // pass our application into our routes
+    var express = require('express');
+    var app = express();
+    var mongoose = require('mongoose');
+    var bodyParser = require('body-parser');
+    var methodOverride = require('method-override');
+    var db = require('./config/db');
+    var port = process.env.PORT || 3000;
 
-// start app ===============================================
-app.listen(port);	
-console.log('Magic happens on port ' + port); 			// shoutout to the user
-exports = module.exports = app; 						// expose app
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(methodOverride('X-HTTP-Method-Override'));
+    app.use(express.static(__dirname + '/public'));
+
+    require('./app/routes')(app);
+
+    app.listen(port);
+
+    console.log('Worker ' + workerId + ' listening on port ' + port);
+
+    exports = module.exports = app;
+}
